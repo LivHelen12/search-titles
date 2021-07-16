17c12cb9cbeeba3c139d3ec733451fc84a9634e1
Livia Helen <liviahelen025@gmail.com>
2021-07-16 14:47:48 -0300

    Finalizaçao dos testes nos componentes

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -6,7 +6,7 @@ export const PostCard = ({ title, body, id, cover }) => (
     <img src={cover} alt={title} />
     <div className="post-content">
       <h2>
-        {title} - {id}
+        {title} {id}
       </h2>
       <p>{body}</p>
     </div>
